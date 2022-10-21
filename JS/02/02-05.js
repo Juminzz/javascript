a04ea74cb8c488687dedf06194d7a471d2bcdb0c
Juminzz <justin4836@naver.com>
2022-10-21 17:50:01 +0900

    10.21

--- JS/02/02-05.js
+++ JS/02/02-05.js
@@ -1,4 +1,11 @@
 let number = Number(prompt("1 ~ 9 사이의 값을 입력하세요."));
+let aa1 = alert('1 ~ 9 사이 값만 적어 주세요 아니면 종료됩니다.')
+aa1 = Number(1,2,3,4,5,6,7,8,9);
+if (aa1=Number){
+    alert('계속')
+}else{
+    alert('종료')
+}
 list='';
 list += `<table><tr><td> &nbsp;&nbsp; ${+number+" x 1 = " +number*1}</td></tr>`;
 list += `<table><tr><td> &nbsp;&nbsp; ${+number+" x 2 = " +number*2}</td></tr>`;
