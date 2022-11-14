d890ab049d703a2106329b8768d8537d87a2afcb
Juminzz <justin4836@naver.com>
2022-11-14 17:45:34 +0900

    11.14

--- js16/js16js/16-04.js
+++ js16/js16js/16-04.js
@@ -1,34 +1,44 @@
     
-    let count = 6;
-    let aa2= 0;
-    let time=0;
+    // let count = 6;
+    // let aa2= 0;
+    // let time=0;
+    // function clicked(){
+    //     // const aa1 = setTimeout(()=>     // 일정시간 지난후 1번만실행
+    //     // console.log('시작')
+    //     // ,500);
+    //     aa2 = setInterval(()=>{     // 일정시간동안 코드 실행
+    //         count--
+    //         console.log(`${count}번째`);
+    //         if(count==0){
+    //             alert('펑 !');
+    //         }
+    //     },1000)
+    //     setTimeout(()=>{
+    //         clearInterval(aa2)
+    //     },6000)
+    // }
+    // function clicked2(){
+    //     clearInterval(aa2)      // 클릭하면 일시정지
+    // }
+    // function clicked3(){
+    //     count==0?
+    //     aa2 = setInterval(()=>{
+    //         count--
+    //         console.log(`${count}번째`);
+    //         if(count==0){
+    //             alert('펑 !');
+    //         }
+    //     },1000):count=6
+    // }
+    
+
+    var count = 5
     function clicked(){
-        // const aa1 = setTimeout(()=>     // 일정시간 지난후 1번만실행
-        // console.log('시작')
-        // ,500);
-        aa2 = setInterval(()=>{     // 일정시간동안 코드 실행
-            count--
-            console.log(`${count}번째`);
-            if(count==0){
-                alert('펑 !');
-            }
-        },1000)
-        setTimeout(()=>{
-            clearInterval(aa2)
-        },6000)
+        setInterval(function(){
+        count--
+        console.log(count)
+     },1000)
     }
     function clicked2(){
-        clearInterval(aa2)      // 클릭하면 일시정지
+        clearInterval(count)
     }
-    function clicked3(){
-        count==0?
-        aa2 = setInterval(()=>{
-            count--
-            console.log(`${count}번째`);
-            if(count==0){
-                alert('펑 !');
-            }
-        },1000):count=6
-    }
-    
-    
